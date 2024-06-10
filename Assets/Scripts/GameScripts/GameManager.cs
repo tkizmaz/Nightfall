a41263cd18b126d5f3cf2a59546c7f705e19ee69
Taha Kızmaz <tahakizmaz@gmail.com>
2024-06-10 19:28:36 +0100

    refactor(scene): Change the scene components

---
+++ Assets/Scripts/GameScripts/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    [SerializeField]
+    private GameObject player;    
+    private void Awake() 
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
