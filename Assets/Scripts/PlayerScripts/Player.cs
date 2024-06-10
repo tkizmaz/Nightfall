6a929d2ac16d00b61a4a53d780e26848042def5a
Taha Kızmaz <tahakizmaz@gmail.com>
2024-06-10 19:02:45 +0100

    feat(code): Add stats to player

---
+++ Assets/Scripts/PlayerScripts/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Health health;
+    private Mana mana;
+    public Health Health
+    {
+        get { return health; }
+        set { health = value; }
+    }
+    public Mana Mana
+    {
+        get { return mana; }
+        set { mana = value; }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.AddComponent<Health>();
+        this.gameObject.AddComponent<Mana>();
+    }
+}
