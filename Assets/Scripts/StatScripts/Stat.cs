e0044d08aee707ae8c4df616666f5990cb435149
Taha Kızmaz <tahakizmaz@gmail.com>
2024-06-19 01:58:54 +0100

    feat(logic): Create event system

--- Assets/Scripts/StatScripts/Stat.cs
+++ Assets/Scripts/StatScripts/Stat.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class StatChangedEvent : UnityEvent<int> {}
 
 public class Stat : MonoBehaviour
 {
+    protected StatChangedEvent onStatChanged;
     private int statValue;
     public int StatValue
     {
         get { return statValue; }
-        set { statValue = value; }
-    }
+        set { statValue = value;
+              onStatChanged.Invoke(statValue);
+        }
+   }
 
     protected virtual void OnStatFinished()
     {
@@ -22,5 +29,16 @@ public class Stat : MonoBehaviour
     void Awake()
     {
         statValue = 100;
+        onStatChanged = new StatChangedEvent();
+    }
+
+    void Start()
+    {
+        GameObject gameController = GameObject.FindWithTag("GameController");
+        GameUI gameUI = gameController.GetComponent<GameUI>();
+        if(gameUI != null)
+        {
+            onStatChanged.AddListener(gameUI.ChangeManaText);
+        }
     }
 }
