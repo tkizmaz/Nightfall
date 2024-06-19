b3f9b9c8303c3cee861cc3e52436074cca710894
Taha Kızmaz <tahakizmaz@gmail.com>
2024-06-19 03:28:35 +0100

    feat(ui): Add ui changes

--- Assets/Scripts/UIScripts/GameUI.cs
+++ Assets/Scripts/UIScripts/GameUI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameUI : MonoBehaviour
 {
@@ -10,9 +11,16 @@ public class GameUI : MonoBehaviour
     private TMPro.TMP_Text manaText;
     [SerializeField]
     private GameObject player;
+    [SerializeField]
+    private Image blinkIcon;
 
     public void ChangeManaText(int mana)
     {
         manaText.text = "Mana: " + mana;
+    }
+
+    public void ChangeAbilityStatus(bool status)
+    {
+        blinkIcon.color = status ? Color.white : Color.grey;
     }
 }
