7b36d2fee9fad8aa17cde1758e0c257dd96ce99d
Taha Kızmaz <tahakizmaz@gmail.com>
2024-06-26 00:08:58 +0100

    feat(logic): Add cube controller for testing

---
+++ Assets/CubeController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    public float speed = 1.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.PingPong(Time.time * 10 * speed, 20), transform.position.y, transform.position.z);
+    }
+}
