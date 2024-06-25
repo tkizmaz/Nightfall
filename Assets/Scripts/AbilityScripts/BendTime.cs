7c766588ebe8d719bbd1b1bbfd20695d5b8ef6fb
Taha Kızmaz <tahakizmaz@gmail.com>
2024-06-26 00:12:08 +0100

    feat(logic): Add bend time

--- Assets/Scripts/AbilityScripts/BendTime.cs
+++ Assets/Scripts/AbilityScripts/BendTime.cs
@@ -4,8 +4,36 @@ using UnityEngine;
 
 public class BendTime : Ability
 {
+    private float slowDownDuration = 5f;
+    private void Awake() 
+    {
+        abilityType = AbilityType.BendTime;
+        manaCost = 50;
+        cooldownDuration = 5f;
+    }
+
+    private void Update() 
+    {
+        PerformAbility();
+    }
+    
     protected override void PerformAbility()
     {
-        Debug.Log("Bend Time performed");
+        bool hasEnoughMana = HasEnoughMana();
+        if(isReadyToPerform && hasEnoughMana && isAbilitySelected)
+        {
+            if(Input.GetMouseButtonDown(1))
+            {
+                StartCoroutine(SlowDownTime());
+                base.PerformAbility();
+            }
+        }
     }
+
+    private IEnumerator SlowDownTime()
+    {
+        Time.timeScale = 0.5f;
+        yield return new WaitForSeconds(slowDownDuration);
+        Time.timeScale = 1f;
+    } 
 }
