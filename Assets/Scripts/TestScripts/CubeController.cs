19f22781fd560bf5f327d20cbc76708450ae0d9b
Taha Kızmaz <tahakizmaz@gmail.com>
2024-06-26 03:28:31 +0100

    feat(logic): Change material to shader

--- Assets/Scripts/TestScripts/CubeController.cs
+++ Assets/Scripts/TestScripts/CubeController.cs
@@ -4,8 +4,29 @@ using UnityEngine;
 
 public class CubeController : MonoBehaviour
 {
+    [SerializeField]
+    private Material sihouetteShader;
+    private Material defaultMaterial;
+    private Renderer cubeRenderer;
     public float speed = 100.0f;
     // Update is called once per frame
+
+    public void ChangeToSilhouette()
+    {
+        cubeRenderer.material = sihouetteShader;
+    }
+
+    public void ChangeToDefault()
+    {
+        cubeRenderer.material = defaultMaterial;
+    }
+
+    private void Start() 
+    {
+        cubeRenderer = GetComponent<Renderer>();
+        defaultMaterial = cubeRenderer.material;
+    }
+
     void Update()
     {
         transform.position = new Vector3(Mathf.PingPong(Time.time * 10 * speed, 20), transform.position.y, transform.position.z);
