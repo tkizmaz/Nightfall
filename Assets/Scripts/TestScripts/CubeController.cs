d08e2ac8c4d134b1f851ddc7207e28989a596026
Taha Kızmaz <tahakizmaz@gmail.com>
2024-07-02 01:52:10 +0100

    feat(asset): Add enemy model

--- Assets/Scripts/TestScripts/CubeController.cs
+++ Assets/Scripts/TestScripts/CubeController.cs
@@ -6,29 +6,32 @@ public class CubeController : MonoBehaviour
 {
     [SerializeField]
     private Material sihouetteShader;
-    private Material defaultMaterial;
-    private Renderer cubeRenderer;
-    public float speed = 100.0f;
-    // Update is called once per frame
+    [SerializeField]
+    List<Material> defaultMaterials;
+    private Renderer enemyRenderer;
 
     public void ChangeToSilhouette()
     {
-        cubeRenderer.material = sihouetteShader;
+        foreach(Material material in defaultMaterials)
+        {
+            enemyRenderer.material = sihouetteShader;
+        }
     }
 
     public void ChangeToDefault()
     {
-        cubeRenderer.material = defaultMaterial;
+        foreach(Material material in defaultMaterials)
+        {
+            enemyRenderer.material = material;
+        }
     }
 
     private void Start() 
     {
-        cubeRenderer = GetComponent<Renderer>();
-        defaultMaterial = cubeRenderer.material;
-    }
-
-    void Update()
-    {
-        transform.position = new Vector3(Mathf.PingPong(Time.time * 10 * speed, 20), transform.position.y, transform.position.z);
+        enemyRenderer = GetComponent<Renderer>();
+        foreach(Material material in defaultMaterials)
+        {
+            defaultMaterials.Add(material);
+        }
     }
 }
