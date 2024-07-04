498199b39c72b2075bd28a735ff55d4d79c204c0
Taha Kızmaz <tahakizmaz@gmail.com>
2024-07-04 21:20:59 +0100

    feat(logic): Add enemy senses

--- Assets/Scripts/EnemyScripts/Enemy.cs
+++ Assets/Scripts/EnemyScripts/Enemy.cs
@@ -3,40 +3,120 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+enum EnemyState
+{
+    Alerted,
+    Idle,
+    Patrol,
+    Chase,
+    Attack
+}
+
 public class Enemy : MonoBehaviour
 {
-    public float viewRadius;
-    public float viewAngle;
-    public LayerMask targetMask;
+    private EnemyState enemyState = EnemyState.Idle;
+    public float visionRange = 10.0f;
+    public float visionAngle = 60.0f;
     public LayerMask obstacleMask;
-
-    public GameObject player;
-
-    [SerializeField]
+    public Transform player;
     private NavMeshAgent navMeshAgent;
-    private Health health;
-    
+    private Animator enemyAnimator;
+    public Transform[] patrolPoints;
+    private int currentPatrolIndex = 0;
+    private bool isEnemyAlerted = false;
+    public bool IsEnemyAlerted{ get;}
+
     void Start()
     {
-        health = GetComponent<Health>();
-    }
-
-    private void Awake() 
-    {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        enemyAnimator = GetComponent<Animator>();
+        SetEnemyPatrol();
     }
 
-    private void Update() 
+    void Update()
     {
-        Vector3 directionToPlayer = (player.transform.position - transform.position).normalized;
-        if(Vector3.Angle(transform.forward, directionToPlayer) < viewAngle / 2)
+        CheckPlayerInSight();
+        CheckIfPlayerDisappeared();
+        CheckPatrolDestinationReached();
+    }
+
+    void CheckPlayerInSight()
+    {
+        Vector3 directionToPlayer = player.position - transform.position;
+        float distanceToPlayer = directionToPlayer.magnitude;
+
+        if (distanceToPlayer <= visionRange)
         {
-            float playerDistance = Vector3.Distance(transform.position, player.transform.position);
-            if(!Physics.Raycast(transform.position, directionToPlayer, playerDistance, obstacleMask))
+            float angleToPlayer = Vector3.Angle(transform.forward, directionToPlayer);
+
+            if (angleToPlayer <= visionAngle / 2)
             {
-                navMeshAgent.SetDestination(player.transform.position);
+                if (!Physics.Raycast(transform.position, directionToPlayer.normalized, distanceToPlayer, obstacleMask))
+                {
+                    SetEnemyToChasingState();
+                    navMeshAgent.SetDestination(player.position);
+                }
+                else
+                {
+                    enemyState = EnemyState.Patrol;
+                }
             }
         }
-
     }
+
+    private void SetEnemyToChasingState()
+    {
+        enemyState = EnemyState.Chase;
+        enemyAnimator.SetBool("isRunning", true);
+        navMeshAgent.speed = 5f;
+        isEnemyAlerted = true;
+    }
+
+    void CheckIfPlayerDisappeared()
+    {
+        if(enemyState == EnemyState.Chase)
+        {
+            float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+            if(distanceToPlayer > visionRange)
+            {
+                SetEnemyPatrol();
+                enemyAnimator.SetBool("isRunning", false);
+            }
+        }
+    }
+
+    void SetEnemyPatrol()
+    {
+        enemyState = EnemyState.Patrol;
+        isEnemyAlerted = false;
+        navMeshAgent.speed = 1f;
+        enemyAnimator.SetBool("isPatrolling", true);
+        SetPatrolDestination();
+    }
+
+    void SetPatrolDestination()
+    {
+        navMeshAgent.SetDestination(patrolPoints[currentPatrolIndex].position);
+    }
+
+    void CheckPatrolDestinationReached()
+    {
+        if(enemyState == EnemyState.Patrol)
+        {
+            if(Vector3.Distance(transform.position, patrolPoints[currentPatrolIndex].position) < 1.0f)
+            {
+                currentPatrolIndex++;
+                if(currentPatrolIndex >= patrolPoints.Length)
+                {
+                    currentPatrolIndex = 0;
+                }
+                SetPatrolDestination();
+            }
+        }
+        else
+        {
+            Debug.Log("Not in patrol state");
+        }
+    }
+
 }
