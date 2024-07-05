58b577a75cb5c154195b158d3b05feaf33550342
Taha Kızmaz <tahakizmaz@gmail.com>
2024-07-05 02:17:57 +0100

    feat(code): Play enemy death animation for testing

--- Assets/Scripts/CombatScripts/Sword.cs
+++ Assets/Scripts/CombatScripts/Sword.cs
@@ -31,7 +31,7 @@ public class Sword : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Enemy"))
         {
-            Debug.Log("hit enemy");
+            other.gameObject.GetComponent<Enemy>().SetStateToDeath();
         }
     }
 
