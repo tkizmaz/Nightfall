ba1dc08bcfb1187acb5b9f88df15041d2e7f351e
Taha Kızmaz <tahakizmaz@gmail.com>
2024-07-06 19:33:07 +0100

    feat(logic): Add finisher check

--- Assets/Scripts/CombatScripts/Sword.cs
+++ Assets/Scripts/CombatScripts/Sword.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-
-
 public class Sword : MonoBehaviour
 {
     private BoxCollider swordCollider;
     public UnityEvent onPlayerSlash;
     public PlayerAnimationController playerAnimationController;
+    private float finisherRange = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -31,16 +30,41 @@ public class Sword : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Enemy"))
         {
+            AudioManager.instance.PlaySwordHitEnemySfx();
             CombatManager.instance.DealDamage(other.gameObject.GetComponent<Enemy>(), 30);
+            StartCoroutine(DisableCollider());
         }
+    }
+
+    private IEnumerator DisableCollider()
+    {
+        yield return new WaitForSeconds(0.5f);
+        swordCollider.enabled = false;
     }
 
     private void CheckAttack()
     {
         if(Input.GetMouseButtonDown(0))
         {
-            swordCollider.enabled = true;
-            onPlayerSlash.Invoke();
+            // Raycast işlemi
+            RaycastHit hit;
+            Vector3 rayOrigin = Camera.main.transform.position;
+            Vector3 rayDirection = Camera.main.transform.forward;
+            if (Physics.Raycast(rayOrigin, rayDirection, out hit, finisherRange))
+            {
+                if (hit.collider.gameObject.CompareTag("Enemy"))
+                {
+                    Debug.Log("Finisher");
+                    return;
+                }
+            }
+            else
+            {
+                AudioManager.instance.PlaySwordSwingSfx();
+                swordCollider.enabled = true;
+                onPlayerSlash.Invoke();
+            
+            }
         }
     }
 }
