36a6367ec54d755207b7bd2f2a9c600ff3ea9b4c
Taha Kızmaz <tahakizmaz@gmail.com>
2024-07-30 00:20:40 +0100

    feat(logic): Add game over conditions

--- Assets/Scripts/GameScripts/GameManager.cs
+++ Assets/Scripts/GameScripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,15 +12,72 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private List<GameObject> enemyList = new List<GameObject>();
     public List<GameObject> EnemyList { get => enemyList ;}
+    [SerializeField]
+    private float timeLimit = 300f;
+    private float timer;
+    private GameUI gameUI;
+
     private void Awake() 
     {
         if(instance == null)
         {
             instance = this;
+            timer = timeLimit;
         }
         else
         {
             Destroy(this.gameObject);
         }
+    }
+
+    public void OnEnemyDeath(GameObject enemy)
+    {
+        enemyList.Remove(enemy);
+        gameUI.UpdateRemainingEnemiesText(enemyList.Count);
+        CheckAllEnemiesDefeated();
+    }
+
+    private void Update()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            gameUI.UpdateTimerText(timer);
+
+            if (timer <= 0)
+            {
+                CheckAllEnemiesDefeated();
+            }
+        }
+    }
+
+    private void CheckAllEnemiesDefeated()
+    {
+        if(enemyList.Count == 0)
+        {
+            gameUI.ActivateGameOverPanel(true);
+        }
+        else
+        {   
+            if(timer <= 0)
+            gameUI.ActivateGameOverPanel(false);
+        }
+    }
+
+    private void Start() 
+    {
+        gameUI = this.gameObject.GetComponent<GameUI>();
+        gameUI.UpdateRemainingEnemiesText(enemyList.Count);
+    }
+
+    public void PlayNextLevel()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+
+    public void OnPlayerDeath()
+    {
+        Debug.Log("Player Died");
+        gameUI.ActivateGameOverPanel(false);
     }
 }
